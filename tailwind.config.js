9ad353927d77792a4d1bd0630745647203a481e6
DU <sangdyjjang@naver.com>
2025-05-14 09:35:27 +0900

    [Fixed]tailwindcss 환경설정

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // JSX 파일 경로 꼭 포함!
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
